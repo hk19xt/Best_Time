1dcba9fc57758040c3d5ca1c1996af7a0a6820cc
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-08-29 23:12:56 -0400

    Update Best_Time.java

--- Best_Time/src/Best_Time/Best_Time.java
+++ Best_Time/src/Best_Time/Best_Time.java
@@ -3,16 +3,17 @@ package Best_Time;
 public class Best_Time {
     public static int bestProfit(int[] numbers) {
 
-        int bestProfit = 0;
-        int startingValue = numbers[0];
+        int bestProfit = 0; // Initialize the best profit
+        int startingValue = numbers[0]; // Initialize the first value in the input array.
 
-        check(numbers);
+        check(numbers); // Check whether the input array is larger and equal to 2
 
         for (int nextValue=1; nextValue<numbers.length; nextValue++){
             if(numbers[nextValue]<startingValue){ //If the starting point value is larger than the next value,
                 startingValue=numbers[nextValue]; //starting point is changed.
             }
-            if(numbers[nextValue]-startingValue>bestProfit && numbers[nextValue]-startingValue>0){
+            if(numbers[nextValue]-startingValue>bestProfit && numbers[nextValue]-startingValue>0){ // Check if the new best profit is larger than the current best profit &&
+                                                                                                   // Check if the best profit is larger than zero.
                 bestProfit = numbers[nextValue] - startingValue;
             }
         }
