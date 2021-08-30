feb33b0c5862ab2085b3602e5e0bf003289953a2
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-08-29 23:15:02 -0400

    Update Best_Time.java

--- Best_Time/src/Best_Time/Best_Time.java
+++ Best_Time/src/Best_Time/Best_Time.java
@@ -1,12 +1,20 @@
 package Best_Time;
 
+/*Author: Hyejin Kim
+  Date: 8/27/2021*/
+
+//This class is Best_Time class to calculate the max profit by buying and selling the stock.
+//The parameter is input numbers by user from the main class.
+
+
+
 public class Best_Time {
     public static int bestProfit(int[] numbers) {
 
         int bestProfit = 0; // Initialize the best profit
         int startingValue = numbers[0]; // Initialize the first value in the input array.
 
-        check(numbers); // Check whether the input array is larger and equal to 2
+        check(numbers); // Check whether the input array is larger than and equal to 2
 
         for (int nextValue=1; nextValue<numbers.length; nextValue++){
             if(numbers[nextValue]<startingValue){ //If the starting point value is larger than the next value,
@@ -21,13 +29,13 @@ public class Best_Time {
         return bestProfit;
     }
 
-
+    //This method is for checking the input array is larger than and equal to 2.
     public static int check (int[] inputs){
 
         if (inputs.length <= 1){
             return 0;
         }
         return 0;
-    }
-}
+    }// check
+}// Best_Time
 
